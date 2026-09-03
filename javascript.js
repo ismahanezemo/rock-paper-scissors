aba0983607486947b50ad2e3130ac97284701420
ismahane Zem <246312172+ismahanezemo@users.noreply.github.com>
2026-09-04 00:55:22 +0100

    initial functions

--- javascript.js
+++ javascript.js
@@ -1,20 +1,22 @@
-function getComputerChoice (max){
-    return Math.floor(Math.random()*max);
+function getComputerChoice (){
+    let max = 3;
+    let choice = Math.floor(Math.random()*max);
+    if (choice===0){
+        return ("rock");
+    }else if (choice===1){
+        return ("paper");
+    }else{
+        return ("scissors");
+    }
 }
-const choice = getComputerChoice(3);
-if (choice=0){
-    console.log("computer chose Rock");
-}else if (choice=1){
-    console.log("computer chose Paper");
-}else {
-    console.log("computer chose Scissors");
-}
+(getComputerChoice());
 
+let choice = prompt("please enter rock, paper or scissors","");
 function getHumanChoice (){
-    let choice = prompt("please enter rock, paper or scissors","");
-    if (choice="rock"){
+    
+    if (choice==="rock"){
         return "rock";
-    }else if (choice="paper"){
+    }else if (choice==="paper"){
         return "paper";
     }else {
         return "scissors";
@@ -22,9 +24,38 @@ function getHumanChoice (){
 }
 console.log(getHumanChoice());
 
-let humanScore = 0 ;
-let computerScore = 0 ;
 
-function PlayRound (ComputerChoice,HumanChoice){
+let humanScore=0;
+let computerScore=0;
+
+function playGame(){   
+    function PlayRound (humanChoice,computerChoice){
+       humanChoice=humanChoice.toLowerCase();
+       if ((humanChoice==="rock"&&computerChoice==="paper")||(humanChoice==="paper"&&computerChoice==="scissors")||(humanChoice==="scissors"&&computerChoice==="rock")){
+        return "you lose!"+" "+computerChoice+" "+"beats"+" "+humanChoice ;
+    }else if (humanChoice===computerChoice){
+        return "It's a tie! you both chose " + " " +humanChoice ;
+    }else{
+        return "you won!"+" "+humanChoice+" "+"beats"+" "+computerChoice ;
+    }
     
 }
+console.log (PlayRound ( getHumanChoice(), getComputerChoice()));  
+}
+playGame(1);
+playGame(2);
+playGame(3);
+playGame(4);
+playGame(5);
+
+
+function score (humanChoice,computerChoice){
+    if (((humanChoice==="rock"&&computerChoice==="paper")||(humanChoice==="paper"&&computerChoice==="scissors")||(humanChoice==="scissors"&&computerChoice==="rock"))){
+        return humanScore++;
+    }else if (((computerChoice==="rock"&&humanChoice==="paper")||(computerChoice==="paper"&&humanChoice==="scissors")||(computerChoice==="scissors"&&humanChoice==="rock"))){
+        return computerScore++;
+    }else{
+        return 
+    } 
+}   
+console.log(score()) ;
