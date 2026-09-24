18ffcb531d73692afe5d5ed4df8ccd18c152ed81
ismahane Zem <246312172+ismahanezemo@users.noreply.github.com>
2026-09-24 16:20:39 +0100

    Add UI for rock-paper-scissors with buttons and score tracking

--- javascript.js
+++ javascript.js
@@ -1,3 +1,10 @@
+const rock = document.querySelector(".rock")
+const paper = document.querySelector(".paper")
+const scissors = document.querySelector(".scissors")
+const results = document.querySelector(".results")
+const score_ = document.querySelector(".score_")
+
+
 function getComputerChoice (){
     let max = 3;
     let choice = Math.floor(Math.random()*max);
@@ -11,27 +18,14 @@ function getComputerChoice (){
 }
 
 
-function getHumanChoice (){
-    let choice = prompt("please enter rock, paper or scissors","");
-    
-    if (choice==="rock"){
-        return "rock";
-    }else if (choice==="paper"){
-        return "paper";
-    }else {
-        return "scissors";
-    }
-}
-
-
-function playGame(){
-    let humanScore=0;
-    let computerScore=0;
+let humanScore=0;
+let computerScore=0;
   
- 
-    function PlayRound (humanChoice,computerChoice){
-       humanChoice=humanChoice.toLowerCase();
-       if ((humanChoice==="rock"&&computerChoice==="paper")||(humanChoice==="paper"&&computerChoice==="scissors")||(humanChoice==="scissors"&&computerChoice==="rock")){
+
+
+function PlayRound (humanChoice,computerChoice){
+    humanChoice=humanChoice.toLowerCase();
+    if ((humanChoice==="rock"&&computerChoice==="paper")||(humanChoice==="paper"&&computerChoice==="scissors")||(humanChoice==="scissors"&&computerChoice==="rock")){
         computerScore = computerScore + 1;
         return "you lose!"+" "+computerChoice+" "+"beats"+" "+humanChoice ;
         
@@ -41,24 +35,61 @@ function playGame(){
         humanScore = humanScore + 1;
         return "you won!"+" "+humanChoice+" "+"beats"+" "+computerChoice ;
     }
+}
     
-}
-
-for (let i=0; i<5 ; i++){
-    console.log(`Round ${i+1}`);
-    console.log(PlayRound(getHumanChoice(), getComputerChoice()));
-    console.log ("Current score"+":"+" "+"you"+" "+"==>"+humanScore+" " ,"computer"+" "+"==>"+ computerScore) ;
-} 
-
-    function score (human,computer){
-        if (human > computer){
-            return "Congratulation ! YOU won the game ";
-        }else if (computer > human){
-            return "YOU lose ! computer wone the game";
-        }else {
-            return "IT'S A TIE ! wanna play again ?";
-        }
+function score (human,computer){
+    if (human > computer){
+       return "Congratulation ! YOU won the game ";
+    }else if (computer > human){
+       return "YOU lose ! computer won the game";
+    }else {
+       return "IT'S A TIE ! wanna play again ?";
+        
     }
-    console.log(score(humanScore,computerScore));
 }
-playGame();
+
+rock.addEventListener("click",()=>{
+    
+    results.textContent=PlayRound("rock",getComputerChoice())
+     score_.textContent="You"+" "+">"+" "+humanScore+" "+"|"+" "+"computer"+" "+">"+" "+computerScore
+
+
+    if ((humanScore===5)||(computerScore===5)){
+        return score_.textContent=score(humanScore,computerScore)
+    }
+    
+})
+
+paper.addEventListener("click",()=>{
+    
+    results.textContent=PlayRound("paper",getComputerChoice())
+    score_.textContent="You"+" "+">"+" "+humanScore+" "+"|"+" "+"computer"+" "+">"+" "+computerScore
+
+
+    if ((humanScore===5)||(computerScore===5)){
+        return score_.textContent=score(humanScore,computerScore)
+    }
+})
+
+scissors.addEventListener("click",()=>{
+    
+    results.textContent=PlayRound("scissors",getComputerChoice())
+    score_.textContent="You"+" "+">"+" "+humanScore+" "+"|"+" "+"computer"+" "+">"+" "+computerScore
+
+    if ((humanScore===5)||(computerScore===5)){
+        return score_.textContent=score(humanScore,computerScore)
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
